22cc03f98763a20758670b0e819c701ebc804f1d
akulsharma1 <akulsharma02@gmail.com>
2025-08-02 14:36:27 -0700

    made server deploy at localhost:3000

--- hermes-site/vite.config.ts
+++ hermes-site/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // 3000 by default
+    host: true, // This allows external connections if needed
+  },
 })
